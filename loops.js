11f32fab33d719c67c8869dc1e7c5c539f77223b
Yu Li <yli0607x@gmail.com>
2018-09-20 16:50:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-function forLoop(array)
+function forLoop(array){
 for (let i = 0; i<25; i++){
   if (i === 1) {
 array.push("I am 1 strange loop.")
@@ -8,67 +8,7 @@ array.push("I am 1 strange loop.")
 }
 }
 return array
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function forLoop(array){
-//   for (let i = 0; i < 25; i++) {
-//     if (i===1) {
-//       array.push("I am 1 strange loop.")
-//     }
-//     else {array.push(`I am ${i} strange loops.`)
-//   }
-// }
-// return array
-// }
-//
-// function whileLoop(n){
-//   while (n > 0) {
-//     console.log(n); --n;
-//   }
-//   return 'done'
-// }
-//
-//
-
-
+}
 
 
 
