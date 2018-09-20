f371bdd3a3a6b69275208830910b223b948eed63
Yu Li <yli0607x@gmail.com>
2018-09-20 16:47:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,22 +1,70 @@
-function forLoop(array){
-  for (let i = 0; i < 25; i++) {
-    if (i===1) {
-      array.push("I am 1 strange loop.")
-    }
-    else {array.push(`I am ${i} strange loops.`)
+function forLoop(array)
+for (let i = 0; i<25; i++){
+  if i === 1 {
+array.push("I am 1 strange loop.")      
   }
-}
-return array
-}
-
-function whileLoop(n){
-  while (n > 0) {
-    console.log(n); --n;
-  }
-  return 'done'
+  else 
+  array.push(`I am ${i} strange loop.`)
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function forLoop(array){
+//   for (let i = 0; i < 25; i++) {
+//     if (i===1) {
+//       array.push("I am 1 strange loop.")
+//     }
+//     else {array.push(`I am ${i} strange loops.`)
+//   }
+// }
+// return array
+// }
+// 
+// function whileLoop(n){
+//   while (n > 0) {
+//     console.log(n); --n;
+//   }
+//   return 'done'
+// }
+// 
+// 
 
 
 
@@ -34,27 +82,27 @@ function whileLoop(n){
 //       array.push(`I am ${i} strange loops.`)
 //     }
 //   }
-//
+// 
 //   return array
 // }
-//
-//
+// 
+// 
 // function whileLoop(n) {
 //   while (n > 0) {
 //     console.log(--n)
 //   }
-//
+// 
 //   return 'done'
 // }
-//
+// 
 // function doWhileLoop(array) {
 //   function maybeTrue() {
 //     return Math.random() >= 0.5
 //   }
-//
+// 
 //   do {
 //     array = array.slice(1)
 //   } while (array.length > 0 && maybeTrue())
-//
+// 
 //   return array
-// }
+// 
