95cac6f6712ee05182ee5d486ba40898bd18552a
Yu Li <yli0607x@gmail.com>
2018-09-20 16:36:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,32 +1,60 @@
-function forLoop(array) {
+function forLoop(array){
   for (let i = 0; i < 25; i++) {
-    if (i === 1) {
+    if (i===1) {
       array.push("I am 1 strange loop.")
-    } else {
-      array.push(`I am ${i} strange loops.`)
     }
+    else {array.push(`I am ${i} strange loops.`)
   }
-
-  return array
+}
+return array
 }
 
-
-function whileLoop(n) {
+function whileLoop(n){
   while (n > 0) {
-    console.log(--n)
+    console.log(n); --n;
   }
-
   return 'done'
 }
 
-function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
 
-  do {
-    array = array.slice(1)
-  } while (array.length > 0 && maybeTrue())
 
-  return array
-}
+
+
+
+
+
+
+
+
+// function forLoop(array) {
+//   for (let i = 0; i < 25; i++) {
+//     if (i === 1) {
+//       array.push("I am 1 strange loop.")
+//     } else {
+//       array.push(`I am ${i} strange loops.`)
+//     }
+//   }
+//
+//   return array
+// }
+//
+//
+// function whileLoop(n) {
+//   while (n > 0) {
+//     console.log(--n)
+//   }
+//
+//   return 'done'
+// }
+//
+// function doWhileLoop(array) {
+//   function maybeTrue() {
+//     return Math.random() >= 0.5
+//   }
+//
+//   do {
+//     array = array.slice(1)
+//   } while (array.length > 0 && maybeTrue())
+//
+//   return array
+// }
